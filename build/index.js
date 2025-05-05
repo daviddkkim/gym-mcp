e30e84ffb22b1680fb11fa4b850a5961463ecf49
david kim <david.kim@richmond.edu>
2025-05-04 23:19:56 -0700

    testing mcp

--- build/index.js
+++ build/index.js
@@ -1,7 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-
 // Create server instance
 const server = new McpServer({
     name: "gym",
@@ -11,37 +10,29 @@ const server = new McpServer({
         tools: {},
     },
 });
-
-server.tool(
-    "log-workout",
-    "Log a workout",
-    {
-        workout: z.array(z.object({
-            exercise: z.string(),
-            sets: z.array(z.object({
-                reps: z.number(),
-                weight: z.number(),
-            })),
-            notes: z.string().optional(),
+server.tool("log-workout", "Log a workout", {
+    workout: z.array(z.object({
+        exercise: z.string(),
+        sets: z.array(z.object({
+            reps: z.number(),
+            weight: z.number(),
         })),
-    },
-    async ({ workout }) => {
-        return {
-            content: [{
+        notes: z.string().optional(),
+    })),
+}, async ({ workout }) => {
+    console.log(`Logging workout: ${JSON.stringify(workout)}`);
+    return {
+        content: [{
                 type: "text",
                 text: `Workout logged: ${JSON.stringify(workout)}`,
             }],
-        };
-    }
-)
-
+    };
+});
 async function main() {
     const transport = new StdioServerTransport();
     await server.connect(transport);
     console.error("Gym MCP Server running on stdio");
 }
-
-
 main().catch((error) => {
     console.error("Fatal error in main():", error);
     process.exit(1);
